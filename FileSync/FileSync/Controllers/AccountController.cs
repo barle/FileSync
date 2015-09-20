d1d01e41c05a236021df0fc19b8d5fec9b376b4e
Barle <Barlevy63@gmail.com>
2015-09-20 17:21:54 +0300

    adding manage users
    adding manage users

--- FileSync/FileSync/Controllers/AccountController.cs
+++ FileSync/FileSync/Controllers/AccountController.cs
@@ -75,7 +75,7 @@ namespace FileSync.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -151,7 +151,7 @@ namespace FileSync.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new FileSyncUser { UserName = model.Email, Email = model.Email, Place = model.Place };
+                var user = new FileSyncUser { UserName = model.UserName, Email = model.Email, Place = model.Place };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
---
+++ FileSync/FileSync/Controllers/ManageUsersController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FileSync.Models;
+using FileSync.DAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace FileSync.Controllers
+{
+    [Authorize]
+    public class ManageUsersController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View(FileSyncDal.GetAllUsers());
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var user = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Place,Email")] FileSyncUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var existUser = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(user.Id);
+                if (existUser == null)
+                    return HttpNotFound();
+
+                existUser.UserName = user.UserName;
+                existUser.Place = user.Place;
+                existUser.Email = user.Email;
+
+                HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().Update(existUser);
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var user = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(id);
+            if (user == null)
+                return HttpNotFound();
+
+            HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>().Delete(user);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // add dispose here
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
