bf29f917376053d0e406cb5b582d3ee9220ea7d1
itkr33 <itkr@walla.co.il>
2015-09-20 22:23:38 +0300

    adding statistics

---
+++ FileSync/FileSync/Controllers/StatisticsController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileSync.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetFilesPerDays()
+        {
+            var results = new List<object>();
+            //foreach
+            results.Add(new { x = "1", y = "10" });
+            results.Add(new { x = "2", y = "30" });
+            results.Add(new { x = "3", y = "25" });
+            results.Add(new { x = "4", y = "35" });
+            results.Add(new { x = "5", y = "3" });
+
+
+            return Json(results.ToArray(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ FileSync/FileSync/Scripts/Statistics/daysToFilesGraph.js
@@ -0,0 +1,71 @@
+﻿var data = [{
+    "sale": "202",
+    "year": "bar"
+}, {
+    "sale": "215",
+    "year": "karavani"
+}, {
+    "sale": "179",
+    "year": "karavani"
+}, {
+    "sale": "199",
+    "year": "bar"
+}/*, {
+    "sale": "134",
+    "year": "2003"
+}, {
+    "sale": "176",
+    "year": "2010"
+}*/];
+
+var vis = d3.select("#visualisation"),
+    WIDTH = 1000,
+    HEIGHT = 500,
+    MARGINS = {
+        top: 20,
+        right: 20,
+        bottom: 20,
+        left: 50
+    },
+
+xScale = d3.scale.ordinal().domain(["bar", "karavani"]).range([MARGINS.left, WIDTH - MARGINS.right]),
+yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([134, 215]);
+
+xAxis = d3.svg.axis()
+    .scale(xScale),
+
+yAxis = d3.svg.axis()
+    .scale(yScale);
+
+vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+    .call(xAxis);
+
+yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
+
+var lineGen = d3.svg.line()
+    .x(function (d) {
+        return xScale(d.year);
+    })
+    .y(function (d) {
+        return yScale(d.sale);
+    });
+
+vis.append('svg:path')
+  .attr('d', lineGen(data))
+  .attr('stroke', 'green')
+  .attr('stroke-width', 2)
+  .attr('fill', 'none');
+
+
+
+
+
